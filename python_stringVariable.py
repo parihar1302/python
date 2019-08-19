c10fec70a6d7346b48276ec599de7ce05fd4eb5a
parihar1302 <ab3parihar@gmail.com>
2019-08-20 00:55:31 +0530

    python aws s3 integration

---
+++ python_stringVariable.py
@@ -0,0 +1,8 @@
+str = "AvengerAssemble"
+
+print (str)
+print ("str")
+print (str[1])    ##indexing :- number start from 0 
+print (str[4:10])    ##range :- including from given number : excluding only last value
+print (str[2:])   ##giving value to last value
+print (str[:4])
