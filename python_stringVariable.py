5b0612643b5661010aad2885a6d496fbbd8f3938
parihar1302 <ab3parihar@gmail.com>
2019-08-20 01:00:03 +0530

    StringVariable

--- python_stringVariable.py
+++ python_stringVariable.py
@@ -5,4 +5,4 @@ print ("str")
 print (str[1])    ##indexing :- number start from 0 
 print (str[4:10])    ##range :- including from given number : excluding only last value
 print (str[2:])   ##giving value to last value
-print (str[:4])
+print (str[:4])    ##from first value to second last value
