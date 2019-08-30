776d3155b8832fde2a7b6d3ee8eab03ee6a71049
parihar1302 <ab3parihar@gmail.com>
2019-08-30 17:01:24 +0530

    Operator_airthumetic_&_comarison

---
+++ python_Operator_Airthmetic_&_Comparison.py
@@ -0,0 +1,32 @@
+#Operration:- Airthmetic & Comparison operator
+num1 = 35
+num2 = 3
+
+ans1 = num1 + num2   #addition
+print (ans1)
+
+ans2 = num1 - num2     #substraction
+print (ans2)
+
+ans3 = num1 * num2      #multiplication
+print (ans3)
+
+ans4 = num1 / num2      #division
+print (ans4)
+
+ans5 = num1 // num2            #give only primary value
+print (ans5)
+
+ans6 = num1 % num2        #give remainder
+print (ans6)
+
+#num1 == num2
+
+#num1 != num2
+
+#num1 > num2
+
+#num1 < num2
+
+#num1 >= num2
+
