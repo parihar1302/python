844ce01562fed6ec6786e8cfcb12f12b3a2cc14e
parihar1302 <ab3parihar@gmail.com>
2019-08-31 15:21:12 +0530

    if_elif_else_comdition

---
+++ python_if_else_condition.py
@@ -0,0 +1,39 @@
+
+#if_else  simple programme
+    
+num = 10
+if num > 50:
+    print ("not good")
+else:
+    print ("very good")
+    
+#########################
+
+#if_else programme with input function
+
+score = int(input("enter the number: "))
+if score < 3:
+    print ("better luck, next time")
+else:
+    print ("well done, pal")
+
+###################################
+
+#if_else programme with input function & comparison operator
+    
+rating = int(input("enter the rating: "))
+if rating == 1:
+    print ("sorry! we appologize")
+elif rating == 2:
+    print ("we will do better")
+elif rating == 3:
+    print ("average service")
+elif rating == 4:
+    print ("good taste")
+elif rating == 5:
+    print ("best of all time")
+else:
+    print ("enter the rating b/w 1/5")
+
+    
+
