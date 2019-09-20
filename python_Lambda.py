a8e61b8ca9a6e98948fa4cbf33438928e62716a6
parihar1302 <ab3parihar@gmail.com>
2019-09-20 23:15:44 +0530

    Lambda_function

---
+++ python_Lambda.py
@@ -0,0 +1,12 @@
+##LAMBDA - is annonymous function
+
+sum = lambda num1, num2: num1 + num2
+print (sum(2, 3))
+
+   or
+
+def sum(num1, num2):
+    count = num1 + num2
+    print (count)
+(sum(2, 3))
+    
